85c7072fbae7a6942b92e5dd3da223e90a6b9825
Chandler <thames.ft@gmail.com>
2016-06-03 22:05:46 -0700

    Update Table.R

--- Table.R
+++ Table.R
@@ -8,3 +8,26 @@ smoke
 # Or we can create the table directly
 smoke <- matrix(c(51,43,22,92,28,21,68,22,9),ncol=3,byrow=TRUE)
 colnames(o) <- c("High","Low","Middle")
+rownames(o) <- c("current","former","never")
+smoke <- as.table(smoke)
+smoke
+
+# Tools for working with tables
+barplot(smoke,legend=T,beside=T,main='Smoking Status')
+plot(smoke,main='Smoking Status By Socioeconomic Status')
+
+# Use margin command
+margin.table(smoke)
+margin.table(smoke,1)
+margin.table(smoke,2)
+
+# Calculate the proportions
+smoke/margin.table(smoke)
+# Or directly using:
+prop.table(smoke)
+
+# Calculate the Chi square
+expected <- as.array(margin.table(smoke,1)) %*% t(as.array(margin.table(smoke,2))) / margin.table(smoke)
+chi <- sum((expected - as.array(smoke))^2/expected)
+chi
+1-pchisq(chi,df=4)
